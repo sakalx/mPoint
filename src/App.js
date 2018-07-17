0b516fc6c54b9591cf85f57993617727bb5eb2b2
sakal <sakals@mail.ua>
2018-07-17 14:27:57 -0400

    init: firebase
    Signed-off-by: sakal <sakals@mail.ua>

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 import Rule from 'root/scenes/rule';
 
+import Slide from '@material-ui/core/Slide';
+
 const App = () => (
   <React.Fragment>
-    <Rule/>
+    <Slide direction="up" in={true} timeout={{enter: 800}}>
+      <Rule/>
+    </Slide>
   </React.Fragment>
 );
 
